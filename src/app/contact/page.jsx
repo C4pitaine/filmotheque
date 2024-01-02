43b3a20f98003d767c4cd5fc9daaba16733a9012
Alex <sacre.alexandre@hotmail.com>
2024-01-02 20:11:29 +0100

    onClick contactForm

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,52 @@
+'use client'
+
 export default function contact() {
+
+    const handleClick = () =>{
+        const input = document.querySelectorAll('input')
+        const textarea = document.querySelector('textarea')
+        const flashContact = document.querySelector('#flashContact')
+
+        let compteur = 0 
+
+        input.forEach(input=>{
+            if(input.value == "")
+            {
+                input.classList.add('inputNotValid')
+            }else{
+                input.classList.remove('inputNotValid')
+                compteur++
+            }
+        })
+
+        if(textarea.value == "")
+        {
+            textarea.classList.add('inputNotValid')
+        }else{
+            textarea.classList.remove('inputNotValid')
+            compteur++
+        }
+
+        if(compteur==4){
+            textarea.value = ""
+
+            input.forEach(input=>{
+                input.value = ""
+            })
+            
+            flashContact.classList.remove('invisible')
+
+        }
+    }
+
+
     return(
         <>
            <div className="flex items-center justify-center p-12">
             <div className="mx-auto w-full max-w-[550px]">
+                <div id="flashContact" className="mb-5 w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#22c55e] outline-none focus:border-[#6A64F1] focus:shadow-md invisible">
+                    Votre message a bien été envoyé !
+                </div>
                 <div className="mb-5">
                     <label
                     htmlFor="name"
@@ -64,8 +108,9 @@ export default function contact() {
                     ></textarea>
                 </div>
                 <div>
-                    <button
+                    <button 
                     className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
+                    onClick={handleClick}
                     >
                     Envoyer
                     </button>
