f2244f6d30bbc61e2d2b1ab8e71dda95140712f2
Alex <sacre.alexandre@hotmail.com>
2024-01-04 19:52:11 +0100

    Affichage des films

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,27 +1,26 @@
 const API_KEY = "2e2c61ff85593ad2cd79c1bd46d0e7fa"
 
-for(let i=500; i<510;i++){
-        // Étape 1: Utilisation de l'API Fetch
-    fetch("https://api.themoviedb.org/3/movie/"+i+"?api_key="+API_KEY)
-    .then(response => {
-        // Étape 2: Vérification du statut de la réponse
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        // Étape 3: Renvoi de la réponse sous forme de JSON
-        return response.json();
-    })
-    .then(data => {
-        // Étape 4: Traitement de la réponse
-        // console.log(data)
-        console.log(data) 
-        //poster_path pour les images 
-        // https://image.tmdb.org/t/p/w500
-    })
-    .catch(error => {
-        console.error('Fetch error:', error);
-    });
+// Étape 1: Utilisation de l'API Fetch
+fetch("https://api.themoviedb.org/3/movie/upcoming?api_key="+API_KEY)
+.then(response => {
+    // Étape 2: Vérification du statut de la réponse
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    // Étape 3: Renvoi de la réponse sous forme de JSON
+    return response.json();
+})
+.then(data => {
+    // Étape 4: Traitement de la réponse
+    // console.log(data)
+    console.log(data) 
+    //poster_path pour les images 
+    // https://image.tmdb.org/t/p/w500
+})
+.catch(error => {
+    console.error('Fetch error:', error);
+});
 
-}
+
 
 
--- src/app/reviews/page.jsx
+++ src/app/reviews/page.jsx
@@ -1,17 +1,32 @@
 
 import { getReviews } from '@/lib/review'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function reviews() {
 
     const reviews = await getReviews()
-    console.log(reviews)
-
     return(
         <>
-            <h1>Reviews</h1>
-            <div>
-                
+            <div className='wrapper'>
+                <h1 className="text-white mt-2 text-2xl">Reviews</h1>
+                <div className="flex flex-wrap justify-center items-center">
+                    {reviews.map((review)=>(
+                        <div key={review.title} className="carte">
+                            <Link href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img  className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={`https://image.tmdb.org/t/p/w500/${review.poster_path}`} alt={`image de ${review.title}`}/>
+                                <div className="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{review.title}</h5>
+                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{review.overview}</p>
+                                </div>
+                            </Link>
+
+                        </div>
+                        
+                    ))}
+                </div>
             </div>
+            
             
             
         </>
