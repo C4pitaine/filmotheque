aecef5fc3a1dae7a15e1d100f2098947746d3855
Alex <sacre.alexandre@hotmail.com>
2024-01-17 15:53:51 +0100

    Image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,7 @@ export default async function Homepage(){
                     {lastReviews.map((review)=>(
                         <div key={review.title} className="carte">
                             <Link href={`/reviews/${review.id}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                                <img  className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={`https://image.tmdb.org/t/p/w500/${review.poster_path}`} alt={`image de ${review.title}`}/>
+                                <Image width="250" height="150" className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={`https://image.tmdb.org/t/p/w500/${review.poster_path}`} alt={`image de ${review.title}`}/>
                                 <div className="flex flex-col justify-between p-4 leading-normal">
                                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{review.title}</h5>
                                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{review.overview}</p>
--- src/app/reviews/[id]/page.jsx
+++ src/app/reviews/[id]/page.jsx
@@ -1,4 +1,5 @@
 import { getFilm } from "@/lib/review"
+import Image from "next/image"
 
 export default async function ReviewPage( {params: {id}}){
     const review = await getFilm(id)
@@ -8,7 +9,7 @@ export default async function ReviewPage( {params: {id}}){
         <>
             <div className='wrapper'>
                 <div className="flex flex-col md:flex-row text-white">
-                    <img className="filmImage" src={`https://image.tmdb.org/t/p/w500/${review.poster_path}`} alt={`image de ${review.title}`} />
+                    <Image width="250" height="150" className="filmImage" src={`https://image.tmdb.org/t/p/w500/${review.poster_path}`} alt={`image de ${review.title}`} />
                     <div className="filmDescription">
                         <h5 className="mb-3 text-2xl">{review.title}</h5>
                         <p className="mb-3">{review.overview}</p>
